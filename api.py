502416b373fd864eacf053bb6e0e9b232d7c82b2
SilvanoChitocaDB7 <83080119+SilvanoChitocaDB7@users.noreply.github.com>
2024-03-30 15:19:32 +0100

    Update api.py

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ data = {
 }
 
 #Cabeçalho da mensagem com as específicações do SMSGateway provider (Authorization, Content-Type)
-headers = {"Authorization" : "Token 357r185f-ac81-4da1-a03f-85e85d5c8657","Content-Type":"application/json"}
+headers = {"Authorization" : "Token 357r185f-ac81-4da1-a03f-85e8576d5c8657","Content-Type":"application/json"}
 
 #Envio da requisição (Endpoint, header, Dados da mensagem no formato JSON)
 response = requests.post(url, headers=headers, json=json.dumps(data))
